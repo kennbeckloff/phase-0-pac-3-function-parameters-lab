6e2ed6091ff8db19be8f7aaa6ee3e5e520a73fae
kennbeckloff <kennbeckloff688@gmail.com>
2022-06-02 14:23:18 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction (name){
+    console.log(`Hi, my name is ${name}.`);
+}
+function introductionWithLanguage (name,language){
+    console.log(`Hi, my name is ${name} and I am learning to program using ${language}.`);
+}
+function introductionWithLanguage (name,language = "JavaScript"){
+    console.log(`Hi, my name is ${name} and I am learning to program using ${language}.`);
+}
+
+
